974899caeedc578f99f3af078a6a1c6dfa9f66e5
YcaroRossi <ycarorossi@gmail.com>
2021-07-15 18:25:41 -0300

    created bank data class and banktest

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/teste/BancoTeste.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank.teste
+
+import one.digitalinnovation.digionebank.Banco
+
+fun main() {
+    val digiOneBank = Banco (nome = "DigiOne", numero = 15)
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+}
