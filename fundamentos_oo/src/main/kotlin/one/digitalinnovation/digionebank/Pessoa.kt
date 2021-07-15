02e40073f80c21e90d6abb66ffb7741a2e42266d
YcaroRossi <ycarorossi@gmail.com>
2021-07-15 19:35:17 -0300

    updated person and employee to abstract class updated bank test class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Ycaro"
-    var cpf: String = "123.123.123-45"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-
-}
-
-fun main() {
-    val ycaro = Pessoa()
-
-    println(ycaro.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
