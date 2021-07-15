4fe361b3b1b674193f82437b582a68c63b62d1dc
YcaroRossi <ycarorossi@gmail.com>
2021-07-15 18:01:15 -0300

    aula sobre membros de classe

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,14 +2,17 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Ycaro"
-
     var cpf: String = "123.123.123-45"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
 }
 
 fun main() {
     val ycaro = Pessoa()
 
-    println(ycaro.nome)
-    println(ycaro.cpf)
+    println(ycaro.pessoaInfo())
 }
